9a2ed0697b1cfedc6c3c12c15eedd32f2d652922
xtdwp <xidapeng99@qq.com>
2021-01-23 21:14:24 +0800

    将id存储改为了vector

--- OpenSubC/geometry.cpp
+++ OpenSubC/geometry.cpp
@@ -1,8 +1,13 @@
 #include "geometry.h"
 
-opensubc::fuelRod::fuelRod(unsigned _id, double _x, double _y, double _r)//给定坐标位置与半径进行初始化
-{
+unsigned opensubc::fuelRod::num = 0;
+unsigned opensubc::gap::num = 0;
+unsigned opensubc::channel::num = 0;
 
+//给定坐标位置与半径进行初始化
+opensubc::fuelRod::fuelRod(unsigned _id, double _x, double _y, double _r):id(_id), x(_x), y(_y),r(_r)
+{
+	++num;
 }
 
 opensubc::fuelRod::~fuelRod()
@@ -10,12 +15,17 @@ opensubc::fuelRod::~fuelRod()
 
 }
 
-opensubc::gap::gap(unsigned rodId0, unsigned rodId2, unsigned channelId0, unsigned channelId1)
-{
 
+opensubc::gap::gap(unsigned rodId0, unsigned rodId1, unsigned channelId0, unsigned channelId1)
+{
+	id = num++;
+	rodId[0] = rodId0;
+	rodId[1] = rodId1;
+	channelId[0] = channelId0;
+	channelId[1] = channelId1;
 }
 
-opensubc::channel::channel(int numOfRods, unsigned* rodIds)//给定一定数量燃料棒构建单通道
+opensubc::channel::channel(int numOfRods, std::vector<unsigned> rodIds)//给定一定数量燃料棒构建单通道
 {
 
 }
--- OpenSubC/geometry.h
+++ OpenSubC/geometry.h
@@ -22,12 +22,12 @@ namespace opensubc {
 //燃料棒
 class opensubc::fuelRod {
 private:
-	static int num;
+	static unsigned num;
 public:
 	unsigned id;
 	double x, y;
 	double r;
-	unsigned* channelIds;
+	std::vector<unsigned> channelIds;
 
 	fuelRod() = default;
 	fuelRod(unsigned _id, double _x, double _y, double _r);//给定坐标位置与半径进行初始化
@@ -37,29 +37,29 @@ public:
 //间隙
 class opensubc::gap {
 private:
-	static int num;
+	static unsigned num;
 public:
 	unsigned id;
 	unsigned rodId[2];
 	unsigned channelId[2];
 
 	gap() = default;
-	gap(unsigned rodId0, unsigned rodId2, unsigned channelId0, unsigned channelId1);
+	gap(unsigned rodId0, unsigned rodId1, unsigned channelId0, unsigned channelId1);
 };
 
 //通道
 class opensubc::channel
 {
 private:
-	static int num;
+	static unsigned num;
 public:
 	unsigned id;
-	unsigned* gapIds;
-	unsigned* rodIds;
-	double* circleLength;//各燃料棒在通道内的浸润周长
+	std::vector<unsigned> gapIds;
+	std::vector<unsigned> rodIds;
+	std::vector<unsigned> circleLength;//各燃料棒在通道内的浸润周长
 
 	channel() = default;
-	channel(int numOfRods, unsigned* rodIds);//给定一定数量燃料棒构建单通道
+	channel(int numOfRods, std::vector<unsigned> rodIds);//给定一定数量燃料棒构建单通道
 	~channel();
 };
 
