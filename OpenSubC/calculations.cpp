57729fb5167b576994033e624bee475aa3b8f661
瑞倪 张 <zrn-17@outlook.com>
2021-01-23 18:01:06 +0800

    完成了几何以及计算的初始化函数部分

--- OpenSubC/calculations.cpp
+++ OpenSubC/calculations.cpp
@@ -1,10 +1,50 @@
 #include "calculations.h"
-
+#include "tinystr.h"
+#include "tinyxml.h"
 #include "capi.h"
+#include "initialize.h"
+#include <vector>
 
 void opensubc::initialize_calculation()
 {
-
+    TiXmlHandle hDoc(&inp);         // hDoc是&inp指向的对象
+    TiXmlElement* p0Elem;            // 指向元素的指针
+    p0Elem = hDoc.FirstChildElement().Element(); //根节点的指针
+    TiXmlHandle hRoot(p0Elem);       // hRoot是根节点
+    TiXmlElement* LCHANLElem = hRoot.FirstChild("GeometricParameter").FirstChild("LCHANL").Element(); //当前指向了LCHANL节点
+    calculation::length= atof(LCHANLElem->GetText());
+    TiXmlElement* NDXElem = hRoot.FirstChild("GeometricParameter").FirstChild("NDX").Element(); 
+    calculation::numOfBlocks = atoi(NDXElem->GetText());
+    TiXmlElement* TINElem = hRoot.FirstChild("OperatingConditions").FirstChild("TIN").Element();
+    calculation::TInitial = atof(TINElem->GetText());
+    TiXmlElement* VINElem = hRoot.FirstChild("OperatingConditions").FirstChild("VIN").Element();
+    calculation::vInitial = atof(VINElem->GetText());
+    TiXmlElement* PINElem = hRoot.FirstChild("OperatingConditions").FirstChild("PIN").Element();
+    calculation::pInitial = atof(PINElem->GetText());
+    TiXmlElement* TSTEPElem = hRoot.FirstChild("OperatingConditions").FirstChild("TSTEP").Element();
+    calculation::tStep = atof(TSTEPElem->GetText());
+    TiXmlElement* TENDElem = hRoot.FirstChild("OperatingConditions").FirstChild("TEND").Element();
+    calculation::tEnd = atof(TENDElem->GetText());
+    TiXmlElement* RODElem = hRoot.FirstChild("GeometricParameter").FirstChild("ROD").Element();
+    TiXmlElement* NRODElem = hRoot.FirstChild("GeometricParameter").FirstChild("NROD").Element();
+    unsigned NROD= atoi(NRODElem->GetText());
+    std::vector<std::vector<double>> q1(NROD,std::vector<double>(calculation::numOfBlocks));//存放面热功率的vector二维数组
+    double Q;
+    std::vector<double*>q0;        //存放一级指针的vector数组
+    int i=0;
+    for (RODElem; RODElem; RODElem = RODElem->NextSiblingElement())//遍历所有的燃料棒（要求输入卡按id顺序填写）
+    {
+        TiXmlHandle ROD(RODElem);
+        TiXmlElement* QElem = ROD.FirstChild("Q").Element();
+        Q= atof(QElem->GetText());
+        for (int j = 0; j < calculation::numOfBlocks; j++)//输入不同轴向分段数的热功率
+        {
+            q1[i][j] = Q;
+        }
+        q0.push_back(&q1[i][0]);          
+        i++;
+    }
+    calculation::q = &q0[0];
 }
 
 void opensubc::finalize_calculation()
